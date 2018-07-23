16a97d43508f55aa2fbb0498a650a7c8c8a15123
Evaldas <surauciusevaldas@gmail.com>
2018-07-23 15:45:03 +0300

    Add project files.

---
+++ MovSite/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MovSite.Startup))]
+namespace MovSite
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
